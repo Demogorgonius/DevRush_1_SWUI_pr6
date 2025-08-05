e0feb43f4cbb0bcc20a5d6951e91395f064e36e3
Demogorgonius <51359795+Demogorgonius@users.noreply.github.com>
2025-08-05 09:25:21 +0300

    Initial Commit

---
+++ DevRush_1_SWUI_pr6/DevRush_1_SWUI_pr6App.swift
@@ -0,0 +1,17 @@
+//
+//  DevRush_1_SWUI_pr6App.swift
+//  DevRush_1_SWUI_pr6
+//
+//  Created by Sergey on 05.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct DevRush_1_SWUI_pr6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DevRush_1_SWUI_pr6Tests/DevRush_1_SWUI_pr6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DevRush_1_SWUI_pr6Tests.swift
+//  DevRush_1_SWUI_pr6Tests
+//
+//  Created by Sergey on 05.08.2025.
+//
+
+import Testing
+@testable import DevRush_1_SWUI_pr6
+
+struct DevRush_1_SWUI_pr6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
